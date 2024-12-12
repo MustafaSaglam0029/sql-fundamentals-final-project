5016270163d42ff876afd67596e29a15c901a8ea
mustafa saglam <mustafasaglam0029@gmail.com>
2024-12-13 00:25:51 +0100

    Added db/meter.sql

--- db/meter.sql
+++ db/meter.sql
@@ -3,21 +3,19 @@ create type smart_collectable_types_enum as enum ('0', '1');
 create table meter
 (
   business_partner_id  varchar(25) ,
-  connection_ean_code  varchar(50) unique,
+  connection_ean_code  varchar(50) ,
   grid_company_code    varchar(25),
   oda_code             varchar(25),
-  meter_number         varchar(25) primary key,
+  meter_number         varchar(25) ,
   smart_collectable    smart_collectable_types_enum,
-  brand                varchar(25)   not null,
+  brand                varchar(25)   null,
   sjv1                 varchar(25)  null,
   sjv2                 varchar(25)  null,
   installation         varchar(25)  null,
   division             varchar(25)  null,
   move_out_date        timestamp null,
-  row_create_datetime  timestamp not null,
-  move_in_date         timestamp not null,
-  constraint fk_business_partner_id
-            foreign key business_partner_id references mandate(business_partner_id)
+  row_create_datetime  timestamp  null,
+  move_in_date         timestamp  null
 );
 
 
