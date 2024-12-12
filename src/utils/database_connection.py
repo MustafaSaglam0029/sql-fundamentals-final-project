3a0fedd800336fbcd2ba6fb6f03fc42dc84e850e
mustafa saglam <mustafasaglam0029@gmail.com>
2024-12-13 00:27:34 +0100

    Added src/utils/database_connection.py

--- src/utils/database_connection.py
+++ src/utils/database_connection.py
@@ -1,12 +1,25 @@
+import psycopg2
 
-
-DB_CONFIG = {
+db_config = {
     "dbname": "postgres",
     "user": "postgres",
     "password": "1234",
     "host": "127.0.0.1",
     "port": "5432"
 }
+
+def conn(db_config):
+        connection = psycopg2.connect(**db_config)
+        return connection
+
+con = conn(db_config)
+con.close()
+
+
+
+
+
+
 
 
 
