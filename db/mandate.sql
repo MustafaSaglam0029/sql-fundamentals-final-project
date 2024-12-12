5c5c3efb6d8d44168f7ddfaf21fca2c1e868bc0e
mustafa saglam <mustafasaglam0029@gmail.com>
2024-12-13 00:16:08 +0100

    Added db/mandate.sql

--- db/mandate.sql
+++ db/mandate.sql
@@ -3,14 +3,14 @@ create type collection_frequency_types_enum as enum ('D', 'I', 'W', 'M');
 
 create table mandate
 (
-   business_partner_id   varchar(25) unique,
+   business_partner_id   varchar(25) ,
    mandate_status        mandate_status_types_enum ,
    collection_frequency  collection_frequency_types_enum,
-   brand                 text not null,
-   row_update_datetime   timestamp not null,
-   row_create_datetime   timestamp not null,
+   brand                 text  null,
+   row_update_datetime   timestamp  null,
+   row_create_datetime   timestamp  null,
    changed_by            text null,
-   mandate_id            varchar(10) primary key,
-   collection_type       varchar (5) not null,
-   metering_consent      varchar(50) not null
+   mandate_id            varchar(10) ,
+   collection_type       varchar (5)  null,
+   metering_consent      varchar(50)  null
 );
