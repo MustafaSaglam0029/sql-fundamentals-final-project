0690ee501def148ffa22a4e5b18e743172242221
mustafa saglam <mustafasaglam0029@gmail.com>
2024-12-13 00:14:48 +0100

    Added main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from src.utils.database_connection import DB_CONFIG
+from src.utils.database_connection import db_config
 from src.mandate_data_script import load_mandate_data
 from src.mandate_data_script import export_mandate_data
 from src.meter_data_script import load_meter_data
@@ -13,19 +13,19 @@ if __name__ == "__main__":
     execute_load = True
 
     if execute_load:
-        load_mandate_data(r"data/mandate_data.json", DB_CONFIG)
+        load_mandate_data(r"data/mandate_data.json", db_config)
 
-    export_mandate_data ("..data/output_mandate_data.json", DB_CONFIG)
-
-    if execute_load:
-        load_meter_data(r"data/mandate_data.json", DB_CONFIG)
-
-    export_meter_data("../data/output_meter_data.json", DB_CONFIG)
+    export_mandate_data ("..data/output_mandate_data.json", db_config)
 
     if execute_load:
-        load_meter_readings_data(r"/data/meter_readings.json", DB_CONFIG)
+        load_meter_data(r"data/mandate_data.json", db_config)
 
-    export_meter_readings_data("../data/output_meter_readings_data.json", DB_CONFIG)
+    export_meter_data("../data/output_meter_data.json", db_config)
+
+    if execute_load:
+        load_meter_readings_data(r"/data/meter_readings.json", db_config)
+
+    export_meter_readings_data("../data/output_meter_readings_data.json", db_config)
 
 
 
